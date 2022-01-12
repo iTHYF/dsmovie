8bb28ca9bc2ca195bcaa02f52c465dccd7ebacd9
Thyfanne Dayane <thyfannenascimento1210@gmail.com>
2022-01-12 20:20:54 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Pagination from 'components/Navbar/Pagination';
 
 function Listing() {
 
     return(
-        <h1>Página de listagem</h1>
-
+        <Pagination/>
     )
 }
 
